fd4fadf7db0884e16f1816972a9e890fd9d89bd5
Alexander <42187739+Kether180@users.noreply.github.com>
2022-12-07 22:56:08 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package searchengine;
+
+import java.io.IOException;
+
+public class Main {
+
+public static void main(String[] args) throws IOException {
+
+new WebServer(WebServer.getPort());
+
+}
+}
